67371f5f5ca6f9b98ab3be559634c1ee42fafd36
amostalong <amostalong@126.com>
2017-01-23 17:56:54 +0800

    Example add

--- Assets/Script/NodeData.cs
+++ Assets/Script/NodeData.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeData : MonoBehaviour {
+
+	/// <summary>
+	/// 1 is well road, 2 - 254 is worse, 255 is impassable
+	/// </summary>
+	public int cost;
+
+	[SerializeField]
+	Image background;
+	[SerializeField]
+	Text text_identify;
+	[SerializeField]
+	Image image_identify;
 
 	// Use this for initialization
 	void Start () {
