a24c9ba2ae8c2ad55e3140c29a53f939f44a239a
QYY <qyy@QYYs-MacBook-Pro.local>
2017-02-09 10:05:22 +0800

    re-write all

---
+++ Assets/Script/CubeManager.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CubePathData
+{
+	public int index;
+	public int x;
+	public int z;
+	public MeshRenderer mesh;
+	public GameObject cube;
+
+	//rang: (0,1]
+	public float cost = 1;
+	public float F = float.MaxValue;
+
+	public CubePathData(int index,int x, int z, GameObject cube, MeshRenderer mesh)
+	{
+		this.index = index;
+		this.x = x;
+		this.z = z;
+		this.cube = cube;
+		this.mesh = mesh;
+	}
+}
+
+
+public class CubeManager : MonoBehaviour {
+
+	enum State
+	{
+		Set_Start,
+		Set_End,
+		None,
+	}
+
+	public Button btn_setstart;
+	public Button btn_setend;
+	public Button btn_generate;
+	public Button btn_ff;
+	public InputField input_x;
+	public InputField input_z;
+	public InputField input_obstacle;
+
+	[SerializeField]
+	Material base_mat;
+	[SerializeField]
+	Material obstacle_mat;
+
+
+	public List<CubePathData> path_datas = new List<CubePathData>();
+	public List<CubePathData> obstacle_datas = new List<CubePathData>();
+
+	public int x;
+	public int z;
+
+	[SerializeField]
+	State state = State.None;
+	[SerializeField]
+	public int start_cube = -1;
+	[SerializeField]
+	public int end_cube = -1;
+
+	FlowField ff;
+
+	public void Awake()
+	{
+		btn_generate.onClick.AddListener (GenerateCubeMap);
+		btn_setend.onClick.AddListener (SetEnd);
+		btn_setstart.onClick.AddListener (SetStart);
+		btn_ff.onClick.AddListener (GenerateFlowField);
+		ff = new FlowField ();
+	}
+
+	void SetStart()
+	{
+		state = state == State.Set_Start ? State.None : State.Set_Start;
+	}
+
+	void SetEnd()
+	{
+		state = state == State.Set_End ? State.None : State.Set_End;
+	}
+
+	void GenerateFlowField()
+	{
+		ff.GenerateFlowField (this, FlowFieldType.Four_Direction);
+	}
+
+//	void OnGUI()
+//	{
+//		if (GUI.Button(new Rect(10, 10, 100, 30), "Generate Cube Map"))
+//		{
+//			GenerateCubeMap ();
+//		}
+//
+//		input_x = GUI.TextField(new Rect(120,10, 50, 30), "X");
+//		input_z = GUI.TextField (new Rect (180, 10, 50, 30), "Y");
+//		input_obstacle = GUI.TextField(new Rect(240,10,50,30), "Obstacle");
+//	}
+
+	private void GenerateCubeMap()
+	{
+		for(int i = 0; i < path_datas.Count; ++i)
+		{
+			Destroy (path_datas [i].cube);
+		}
+
+		path_datas.Clear ();
+
+		for(int i = 0; i < obstacle_datas.Count; ++i)
+		{
+			Destroy (obstacle_datas [i].cube);
+		}
+
+		obstacle_datas.Clear ();
+
+		start_cube = -1;
+		end_cube = -1;
+		state == State.None;
+
+		x = int.Parse (string.IsNullOrEmpty (input_x.text) ? "1" : input_x.text);
+		z = int.Parse (string.IsNullOrEmpty (input_z.text) ? "1" : input_z.text);
+		int ob = int.Parse (string.IsNullOrEmpty (input_obstacle.text) ? "0" : input_obstacle.text);
+
+		for (int m = 0;m < z; ++m)
+		{
+			for (int n = 0; n < x; ++n) {
+				var go = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				go.transform.SetParent (this.transform);
+				go.transform.localPosition = new Vector3 (n, 0, m);
+				go.transform.localScale = new Vector3 (0.95f, 1f, 0.95f);
+				go.tag = "Path_Cube";
+				go.name = "Paht: " + n.ToString () + "-" + m.ToString ();
+				MeshRenderer mr = go.GetComponent<MeshRenderer> ();
+				mr.sharedMaterial = base_mat;
+				path_datas.Add (new CubePathData (n + m * x, n, m, go, go.GetComponent<MeshRenderer>()));
+			}
+		}
+
+		while(ob > 0)
+		{
+			int ox = Random.Range (0, x);
+			int oz = Random.Range (0, z);
+
+			int ob_index = ox + oz * x;
+			var go = path_datas [ob_index].cube;
+			go.name = go.name + " obstacle";
+			MeshRenderer mr = go.GetComponent<MeshRenderer> ();
+			mr.sharedMaterial = obstacle_mat;
+			//mr.material.color = Color.red;
+			obstacle_datas.Add (new CubePathData (ob, ox, oz, go, mr));
+			--ob;
+		}
+	}
+
+
+
+	void Update()
+	{
+		if (EventSystem.current.IsPointerOverGameObject(fingerID) == false &&state != State.None && Input.GetMouseButtonUp(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit hit_info;
+
+			if(Physics.Raycast (ray, out hit_info))
+			{
+				if (hit_info.transform.tag == "Path_Cube")
+				{
+					Debug.Log ("Click On Path: " + hit_info.transform.name);
+					Debug.Log ("Click On Path_index: " + hit_info.transform.GetSiblingIndex());
+
+					CubePathData cube = path_datas[hit_info.transform.GetSiblingIndex()];
+				
+					if (state == State.Set_Start)
+					{
+						if (start_cube == cube.index) {
+							start_cube = -1;
+							cube.mesh.material = null;
+							cube.mesh.sharedMaterial = base_mat;
+						} else {
+							start_cube = cube.index;
+							cube.mesh.material.color = Color.green;
+						}
+
+					}
+					else if (state == State.Set_End)
+					{
+						if (end_cube == cube.index) {
+							end_cube = -1;
+							cube.mesh.material = null;
+							cube.mesh.sharedMaterial = base_mat;
+						} else {
+							end_cube = cube.index;
+							cube.mesh.material.color = Color.blue;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+
+}
---
+++ Assets/Script/FlowField.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlowFieldType
+{
+	Four_Direction = 4,
+
+	Eight_Direcion = 8,
+}
+
+public class Direction
+{
+	public int x_move;
+	public int z_move;
+
+	public Direction(int x, int z)
+	{
+		x_move = x;
+		z_move = z;
+	}
+}
+
+public class FlowField {
+
+
+	List<CubePathData> open_cube = new List<CubePathData>();
+	List<CubePathData> close_cube = new List<CubePathData> ();
+
+	Direction[] direction;
+	float max_F = 0;
+
+	public void GenerateFlowField(CubeManager cm, FlowFieldType type)
+	{
+		if (cm.end_cube == -1)
+		{
+			Debug.LogError("Please Set End Cube!");
+			return;
+		}
+
+		if (cm.x == 1 || cm.z == 1)
+		{
+			Debug.LogError("No need Flow Field, there is only one way");
+		}
+
+		max_F = 0;
+
+		//left, leftup, up, rightup, right, rightdown, down, leftdown
+		direction = new Direction[8] {
+			new Direction(-1,0), new Direction(0,1), new Direction(1,0), new Direction(0,-1),
+			new Direction(-1,1), new Direction(1,1), new Direction(1,-1), new Direction(-1,-1)
+		};
+
+		open_cube.Clear ();
+		close_cube.Clear ();
+
+		open_cube.Add (cm.path_datas [cm.end_cube]);
+
+		cm.path_datas [cm.end_cube].F = 0;
+
+		//防止死循环
+		int n = 10000;
+
+
+		while(open_cube.Count > 0 && --n > 0)
+		{
+			for(int i = 0; i < open_cube.Count; ++i)
+			{
+				CheckF (open_cube [i], cm, type);
+			}
+		}
+
+		for (int i = 0; i < cm.path_datas.Count; ++i)
+		{
+			cm.path_datas [i].mesh.material.color = new Color (1, 1, 1f - cm.path_datas [i].F / max_F);
+		}
+	}
+
+	private void CheckF(CubePathData end_cube, CubeManager cm, FlowFieldType type)
+	{
+		for (int i = 0; i < (int)type; ++i)
+		{
+			if (end_cube.x + direction [i].x_move < cm.x && end_cube.x + direction [i].x_move > 0
+				&& end_cube.z + direction [i].z_move < cm.z && end_cube.z + direction [i].z_move > 0) {
+
+				int index = end_cube.index + direction [i].x_move + direction [i].z_move * cm.x;
+				
+				if (index > 0 && index < cm.path_datas.Count) {
+					var n_cube = cm.path_datas [index];
+
+					float F = end_cube.cost * 0.5f + n_cube.cost * 0.5f + end_cube.F;
+
+					if (n_cube.F > F) {
+						n_cube.F = F;
+						if (F > max_F)
+							max_F = F;
+					}
+
+					if (!open_cube.Contains (n_cube) && !close_cube.Contains (n_cube)) {
+						open_cube.Add (n_cube);
+					}
+				}
+			}
+		}
+
+		open_cube.Remove (end_cube);
+		close_cube.Add (end_cube);
+	}
+}
